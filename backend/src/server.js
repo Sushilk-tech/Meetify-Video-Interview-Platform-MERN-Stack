f4fd6b0f6c01babe350fe1df01528fa8b513a078
SushilK-tech <sushilk.tech26@gmail.com>
2026-04-03 04:19:25 +0530

    remove some line of code in server js

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,14 +30,14 @@ app.get("/books", (req, res) => {
 })
 
 // make our app ready for deployment
-if (ENV.NODE_ENV) {
-    // app.use(express.static(path.join(__dirname, "..frontend/dist")))
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+// if (ENV.NODE_ENV) {
+//     // app.use(express.static(path.join(__dirname, "..frontend/dist")))
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
-}
+//     app.get("/{*any}", (req, res) => {
+//         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+//     })
+// }
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "success from backend" })
 })
