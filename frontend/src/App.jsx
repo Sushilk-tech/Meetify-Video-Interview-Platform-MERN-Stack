f50eddea16ad37ddc817c8130601a19a6857e8b2
SushilK-tech <sushilk.tech26@gmail.com>
2026-04-07 02:28:54 +0530

    create a file dashboardpage and add some images under assets and creating HomePage or Landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,12 @@ import { Route, Routes, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
 import { Toaster } from 'react-hot-toast'
+import DashboardPage from './pages/DashboardPage'
 
 const App = () => {
   const { isSignedIn, isLoaded } = useUser()
 
-  // 🔥 jab tak clerk load nahi hota kuch mat dikhao
+  //  jab tak clerk load nahi hota kuch mat dikhao
   if (!isLoaded) {
     return <div>Loading...</div>
   }
@@ -16,7 +17,9 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to='/dashboard' />} />
+        <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to='/' />} />
+
         <Route
           path='/problems'
           element={
@@ -30,31 +33,3 @@ const App = () => {
 }
 
 export default App
-// tw, daisyui, react-router, react-hot-toast
-// todo: react-query aka tanstack query, axios
-// ! mkdnksandka
-// ? mkdnkasandka
-// * mkdnkasandka
-
-
-// import React from 'react'
-// import { useUser } from '@clerk/clerk-react'
-// import { Route, Routes, Navigate } from 'react-router-dom'
-// import HomePage from './pages/HomePage'
-// import ProblemsPage from './pages/ProblemsPage'
-// import { Toaster } from 'react-hot-toast'
-
-// const App = () => {
-//   const { isSignedIn } = useUser()
-//   return (
-//     <>
-//       <Routes>
-//         <Route path='/' element={<HomePage />} />
-//         <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
-//       </Routes>
-//       <Toaster />
-//     </>
-//   )
-// }
-
-// export default App
